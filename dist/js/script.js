58025ca769ad887c31617d89410981044035196a
ReihanZakya <rezakya41@gmail.com>
2023-10-24 15:20:37 +0700

    portfolio with tailwind css3

---
+++ dist/js/script.js
@@ -0,0 +1,64 @@
+//Hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+//klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+//Navbar fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+//preloader
+const loader = document.querySelector("#preloader");
+
+// Menggunakan event 'load' untuk memastikan semua sumber daya halaman telah dimuat
+window.addEventListener("load", function () {
+  // Menunggu 2 detik sebelum menghilangkan preloader
+  setTimeout(function () {
+    loader.style.display = "none";
+  }, 2000); // 2000 milidetik (2 detik)
+});
+
+//dark mode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+//mindahin posisi toggle
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
